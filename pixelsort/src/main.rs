6988e0e72cc3e967a6f0d83a36750bf1f4caceda
kimierik <kimi.malkamaki@gmail.com>
2023-05-08 11:17:20 +0300

    added a secondary sorting method. it does not implement hue calculations properly though

--- pixelsort/src/main.rs
+++ pixelsort/src/main.rs
@@ -1,7 +1,16 @@
 
-use std::{cmp::Ordering, ops::{Index, IndexMut}};
+use std::{cmp::Ordering, ops::{Index, IndexMut}, path::Display};
 
 use image::ImageBuffer;
+
+
+
+#[derive(Debug,Clone, Copy, PartialEq)]
+enum SortMethod{
+    Vibrance,
+    Hue,
+}
+
 
 
 #[derive(Eq)]
@@ -33,15 +42,25 @@ impl PartialEq for Piece {
 struct SortParams{
     pub max_mask:u8,
     pub min_mask:u8,
+    pub sorting_method:SortMethod,
 }
 
 impl Default for SortParams {
     fn default() -> Self {
         Self { 
             max_mask: 250,
-            min_mask: 100 
+            min_mask: 100 ,
+            sorting_method:SortMethod::Vibrance,
         }
     }
+}
+
+
+impl std::fmt::Display for SortMethod {
+    fn fmt(&self,f:&mut std::fmt::Formatter<'_>)->std::fmt::Result{
+        write!(f,"{:?}",self)
+    }
+    
 }
 
 
@@ -56,7 +75,7 @@ struct AppWidget{
 impl Default for AppWidget{
     fn default() -> Self {
         let img = image::open("./images/sakura_samurai.png").unwrap();
-        let mut asdf=img.clone().into_rgb8();
+        let asdf=img.clone().into_rgb8();
         Self { 
             filename: "./images/sakura_samurai.png".to_string(),
             image:asdf ,
@@ -130,7 +149,7 @@ impl AppWidget {
             }
 
             //sort
-            let sorted_thing=sort_pixel_vector(buffer);
+            let sorted_thing=self.sort_pixel_vector(buffer);
 
             //reassign
             let mut ind=0;
@@ -145,11 +164,38 @@ impl AppWidget {
             }
         }
         self.image=asdf;
-
-
     }
 
+    fn sort_pixel_vector(&self,vector:Vec<image::Rgb<u8>>)->Vec<image::Rgb<u8>>{
+        let mut secvec:Vec<Piece>=vec![];
 
+        let mut i=0;//cahgne to enumberage
+        for item in vector.iter(){
+
+            //let total = get_luminance(item);
+            let total = match self.sort_params.sorting_method {
+                SortMethod::Vibrance=>get_luminance(item),
+                SortMethod::Hue=>get_hue(item),
+                
+            };
+
+
+            let a =Piece{val:total,index:i};
+            secvec.push(a);
+            i+=1;
+        }
+
+
+        secvec.sort(); 
+        
+        let mut retvec:Vec<image::Rgb<u8>>=vec![];
+        for (_ind,item) in secvec.iter().enumerate(){
+            retvec.push(vector.index(item.index as usize).clone());
+        }
+
+
+        retvec
+    }
 
 }
 
@@ -164,54 +210,70 @@ fn get_luminance(pixel:&image::Rgb<u8>)->u8{
 }
 
 
+//this does not get hue accurately.. at all but the results are kinda cool
+fn get_hue(pixel:&image::Rgb<u8>)->u8{
+    let r=pixel.0[0] / 255;
+    let g=pixel.0[1] / 255;
+    let b=pixel.0[2] / 255;
+    let mut arra=[r,g,b];
 
-fn sort_pixel_vector(vector:Vec<image::Rgb<u8>>)->Vec<image::Rgb<u8>>{
-    let mut secvec:Vec<Piece>=vec![];
+    arra.sort();
 
-    let mut i=0;//cahgne to enumberage
-    for item in vector.iter(){
-
-        let total = get_luminance(item);
-
-        let a =Piece{val:total,index:i};
-        secvec.push(a);
-        i+=1;
+    let mut hue:u8=0;
+    //what is biggest and what is smalles
+    //put in array and sort
+    //
+    if arra[2]-arra[0]==0 {
+        return 0
     }
 
-
-    secvec.sort(); 
-    
-    let mut retvec:Vec<image::Rgb<u8>>=vec![];
-    for (_ind,item) in secvec.iter().enumerate(){
-        retvec.push(vector.index(item.index as usize).clone());
+    if arra[0]==r {
+        hue = (g-b)/(arra[2]-arra[0])
     }
 
+    if arra[0]==g{
+        hue = 2+(b-r)/(arra[2]-arra[0])
+    }
 
-    retvec
+    if arra[0]==b{
+        hue = 4+(r-g)/(arra[2]-arra[0])
+    }
+    hue
 }
+
+
 
 
 
 impl eframe::App for AppWidget{
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("My egui Application");
+            ui.heading("image pixel sorter");
 
             ui.horizontal(|ui| {
-                let name_label = ui.label("Your name: ");
+                let name_label = ui.label("FileName: ");
                 ui.text_edit_singleline(&mut self.filename)
                     .labelled_by(name_label.id);
             });
 
-            ui.add(egui::Slider::new(&mut self.sort_params.min_mask, 0..=120).text("min"));
-            ui.add(egui::Slider::new(&mut self.sort_params.max_mask, 0..=120).text("max"));
+            ui.heading("Pixel Sorting");
+            ui.add(egui::Slider::new(&mut self.sort_params.min_mask, 0..=255).text("min"));
+            ui.add(egui::Slider::new(&mut self.sort_params.max_mask, self.sort_params.min_mask..=255).text("max"));
+            
+            egui::ComboBox::from_label("sorting method")
+                   .selected_text(format!("{}",self.sort_params.sorting_method))
+                   .show_ui(ui, |ui|{
+                       ui.selectable_value(&mut self.sort_params.sorting_method, SortMethod::Vibrance, "vib");
+                       ui.selectable_value(&mut self.sort_params.sorting_method, SortMethod::Hue, "hue");
+                   });
 
-            if ui.button("saeevach ").clicked() {
-                self.save_image();
-            }
             if ui.button("sort ").clicked() {
                 self.sort_image();
+                self.save_image();
             }
+
+
+
 
             //ui.label(format!("Hello '{}', age {}", self.name, self.age));
             if ui.button("close").clicked(){
